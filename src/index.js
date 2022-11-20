70fcb5d6b4efca42de65888074afc1b70ac282fd
Haotian Huang <haotianhuang@gmail.com>
2022-11-20 19:18:20 +1100

    highlight winning squares

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 
 function Square(props) {
     return (
-        <button className="square" onClick={props.onClick}>
+        <button className="square" id = {props.id} onClick={props.onClick}>
             {props.value}
         </button>
     );
@@ -15,7 +15,8 @@ class Board extends React.Component {
 
     renderSquare(i) {
         return (
-        <Square  
+        <Square 
+            id = {i}
             value={this.props.squares[i]}
             onClick={() => this.props.onClick(i)}
         />
@@ -62,6 +63,11 @@ class Game extends React.Component {
     }
 
     handleClick(i) {
+
+
+
+
+
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
@@ -69,6 +75,10 @@ class Game extends React.Component {
 
         if (calculateWinner(squares) || squares[i]) {
             return;
+        } else {
+            for (let i = 0; i < 9; i++) {
+                document.getElementById(i).style.backgroundColor = "white";
+            }
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
@@ -119,7 +129,11 @@ class Game extends React.Component {
 
         let status;
         if (winner) {
-            status = 'Winner: ' + winner;
+            status = 'Winner: ' + winner[0];
+            // Highlight winning squares
+            for (let i = 0; i < 3; i++) {
+                document.getElementById(winner[1][i]).style.backgroundColor = "yellow";
+            }
         } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
@@ -156,7 +170,8 @@ function calculateWinner(squares) {
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
+            console.log(i);
+            return [squares[a], lines[i]];
         }
     }
     return null;
@@ -172,7 +187,7 @@ function calculateCoordinates(index) {
         [0,1],[1,1],[2,1],
         [0,2],[1,2],[2,2]
     ];
-    
+
     return coordinates[index];
 
 }
